f5799b2fe8487dbd06bfefb363c0a30b161ab1e7
Faruk D. <fdiblen@universe>
2025-04-03 15:51:44 +0200

    add test for indicator validation

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -1,14 +1,14 @@
-import urllib.request, json 
+import urllib.request, json
 from pathlib import Path
 from jsonschema import validate
 import glob
 
 def test_indicators():
-    indicator_files = []
     with urllib.request.urlopen("https://raw.githubusercontent.com/EVERSE-ResearchSoftware/schemas/refs/heads/main/quality_indicators/0.0.1/context.jsonld") as schema_url:
         schema = json.load(schema_url)
-        print("", schema)
-        for indicator_file in glob.glob("indicators/*.json"):
-            print("Validating :", indicator_file)  
-            assert validate(instance=indicator_file, schema=schema) is None
-          
+        # print("Schema:", json.dumps(schema, indent=2))
+        for indicator_file_name in glob.glob("indicators/*.json"):
+            print("Validating :", indicator_file_name)
+            indicator_instance = json.load(open(indicator_file_name))
+            print(validate(instance=indicator_instance, schema=schema))
+            assert validate(instance=indicator_instance, schema=schema) is None
